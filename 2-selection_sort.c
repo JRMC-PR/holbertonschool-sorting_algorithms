2f6ba359b50902af4600e41038dbe3ca72f1d7c5
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-16 22:46:25 +0000

    2 outline

---
+++ 2-selection_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ *selection_sort - prefor the selection sort alg
+ *@array: pointer to array
+ *@size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+
+} /*end selectio sort*/
