0961200e1bb2266f163bd4e97c79d6fb85f23f07
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-16 23:47:49 +0000

    2 selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,5 +6,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	/*Declaratios*/
+	unsigned long int  tmp, i = 0, j = 0, min;
 
+	if (array == NULL || size < 2)
+		return;
+	/*Start comparing values*/
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		} /*end inner for*/
+			if (min != i)
+			{
+				tmp = array[i];
+				array[i] = array[min];
+				array[min] = tmp;
+				print_array(array, size);
+			} /*end swap if*/
+	} /*end outer for*/
 } /*end selectio sort*/
