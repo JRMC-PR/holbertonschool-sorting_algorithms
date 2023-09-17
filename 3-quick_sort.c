85c220b4bba2b457dbc558d15566e0d377aa5e5d
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-17 23:22:17 +0000

    betty fixing

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,11 +6,11 @@
  */
 void quick_sort(int *array, size_t size)
 {
+	/*validate size*/
 	if (size < 2)
 		return;
-
 	quick_sort_array(array, 0, size - 1, size);
-}
+} /*end quick sort*/
 
 /**
  *quick_sort_array - performs alg
@@ -22,15 +22,16 @@ void quick_sort(int *array, size_t size)
 
 void quick_sort_array(int *array, int min, int last, size_t size)
 {
+	/*Declaratios*/
 	int pivot;
-
+	/*check vales*/
 	if (min < last)
 	{
 		pivot = partition(array, min, last, size);
 		quick_sort_array(array, min, pivot - 1, size);
 		quick_sort_array(array, pivot + 1, last, size);
-	}
-}
+	} /*end if */
+} /*end quick sort array*/
 
 /**
  *partition - performs alg
@@ -43,10 +44,11 @@ void quick_sort_array(int *array, int min, int last, size_t size)
 
 int partition(int *array, int min, int last, size_t size)
 {
+	/*Declaratios*/
 	int pivot = array[last];
 	int i = min - 1;
 	int j;
-
+	/*travers the partitio*/
 	for (j = min; j <= last - 1; j++)
 	{
 		if (array[j] <= pivot)
@@ -55,13 +57,13 @@ int partition(int *array, int min, int last, size_t size)
 			swap(&array[i], &array[j]);
 			if (i != j)
 				print_array(array, size);
-		}
-	}
+		} /*end if*/
+	} /*end for*/
 	swap(&array[i + 1], &array[last]);
 	if (i + 1 != j)
 		print_array(array, size);
 	return (i + 1);
-}
+} /*end partition*/
 
 /**
  *swap - performs alg
@@ -74,4 +76,4 @@ void swap(int *a, int *b)
 	int temp = *a;
 	*a = *b;
 	*b = temp;
-}
+} /*end swap*/
